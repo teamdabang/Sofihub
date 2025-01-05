fb0e2d142dfc3009c776fadab5920f8bb5e3938a
teamdabang <dgjg45768@gmail.com>
2025-01-05 17:44:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -21,7 +21,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', 'https://envs.sh/zgQ.jpg')).split() #SAMPLE PIC
+PICS = (environ.get('PICS', 'https://envs.sh/rxe.jpg')).split() #SAMPLE PIC
 NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "https://graph.org/file/60e8a622b14796e4448ce.mp4")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
